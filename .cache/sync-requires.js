c6c91a73c4878d4a999500f02f7e29f254eb2ffa
Vinicius Soranço <vinicius.soranco@levty.com>
2020-08-11 22:18:26 -0300

    .

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/vinicius/code/mysite/.cache/dev-404-page.js"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/home/vinicius/code/mysite/src/pages/index.tsx"))),
+  "component---src-templates-project-tsx": hot(preferDefault(require("/home/vinicius/code/mysite/src/templates/project.tsx")))
+}
+
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -14,8 +14,8 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    min-height: 100vh;
-    min-height: -webkit-fill-available;
+    min-height: 100%;
+    /* min-height: -webkit-fill-available; */
   }
   html {
     height: -webkit-fill-available;
