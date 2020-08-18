9add57e73bb023f54e9d9825cc819a56714c4114
Vinicius Soranço <vinicius.soranco@levty.com>
2020-08-17 21:00:59 -0300

    teste

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import Div100vh from 'react-div-100vh'
+
 export { wrapRootElement } from "./gatsby/wrapRootElement";
+
+export const wrapPageElement = ({ element }) => {
+  return <Div100vh style={{minHeight: '50rvh'}}>{element}</Div100vh>
+}
